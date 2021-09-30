89da65456b4b3aa18b1947fd2792cb1574778e60
MattToast <matthew.drozt@gmail.com>
2021-09-30 16:58:43 -0400

    init an express file structure

---
+++ waveBackend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/api', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/', (req, res) => { res.redirect('/api'); })
+
+module.exports = app;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+});
