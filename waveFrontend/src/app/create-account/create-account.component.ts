61e7ce45de4eda8644abfd25b429f80c8a4f3742
MattToast <matthew.drozt@gmail.com>
2021-10-07 14:44:01 -0400

    User auth from frontend works now

--- waveFrontend/src/app/create-account/create-account.component.ts
+++ waveFrontend/src/app/create-account/create-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 // import { ConsoleLogger } from '@aws-amplify/core';
@@ -23,23 +23,33 @@ export class CreateAccountComponent implements OnInit {
 
 
 
-    getErrorMessageEmail() {
-      if (this.email.hasError('required')) {
-        return 'You must enter a value';
-      }
+  getErrorMessageEmail() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
 
-     return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  async register() {
+    let user = {
+      displayName: this.displayName.value,
+      password: this.password.value,
+      email: this.email.value,
+      username: this.username.value,
     }
-    register() {
-      let user = {
-        displayName:this.displayName.value,
-        password: this.password.value,
-        email: this.email.value,
-        username: this.username.value,
-      }
-      this._userServive.registerUser(user).subscribe((data: any) => {console.log(data)})
-    }
-  constructor(private _userServive: UserService, private http:HttpClientModule ,private route: ActivatedRoute,private router: Router) { }
+
+    await fetch('/auth/local/register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+
+    this._userServive.registerUser(user).subscribe((data: any) => { console.log(data) })
+  }
+
+  constructor(private _userServive: UserService, private http: HttpClientModule, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
