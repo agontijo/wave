588029cdbf500d39170cad625669b32d56260644
MattToast <matthew.drozt@gmail.com>
2021-11-01 14:02:52 -0400

    add route to refresh spotify toks

--- waveBackend/routes/api/spotify/spotify.js
+++ waveBackend/routes/api/spotify/spotify.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const qs = require('qs');
 
 const isAuth = require('../../../middleware/isAuth.js');
+const userActions = require('../../../database/userActions.js')
 
 const router = express.Router();
 
@@ -11,14 +13,12 @@ router.get(
   isAuth.isSpotify,
   async (req, res) => {
     try {
-      console.log('yo')
       const response = await axios.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${req.user.spotifyTok.accessToken}`
         }
       })
-      console.log(response);
       res.send(response.data)
     } catch (e) {
       console.error(e);
@@ -26,5 +26,37 @@ router.get(
     }
   }
 );
+
+
+router.get(
+  '/refresh',
+  isAuth.isLoggedIn,
+  isAuth.isSpotify,
+  async (req, res) => {
+    try {
+      const response = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: qs.stringify({
+          'grant_type': 'refresh_token',
+          'refresh_token': req.user.spotifyTok.refreshToken
+        }),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')}`
+        }
+      });
+
+      // console.log(response.data.access_token);
+      userActions.setSpotifyToks(req.user.uname, response.data.access_token, req.user.spotifyTok.refreshToken);
+      res.send(req.user);
+    } catch (e) {
+      // console.error(e.response.data);
+      console.error(e);
+      res.status(500).send('Something went wrong with spotify');
+    }
+  }
+);
+
 
 module.exports = router;
