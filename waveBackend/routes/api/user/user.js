240be79a026ed010dd539e5a9d7a88395d8ce9fd
Alice Gontijo Goulart Leite <42977445+agontijo@users.noreply.github.com>
2021-11-05 10:34:13 -0400

    change delete user redirect

--- waveBackend/routes/api/user/user.js
+++ waveBackend/routes/api/user/user.js
@@ -79,7 +79,7 @@ router.post(
     }
     try {
       await userActions.deleteUser(req.user.uname);
-      res.redirect('/');
+      res.redirect('/auth/logout');
       // res.sendStatus(204);
     } catch (err) {
       res.status(500).send(err.message)
